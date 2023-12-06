7ae0dbb737c655ac929b2463eaf20c6c9d6c708f
Emily Lazenby <124580748+elaz345@users.noreply.github.com>
2023-12-06 18:07:56 +0000

    Initial

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be positive.")
     list = []
+    num = 2
+
+    while len(list) < number_of_primes:
+        is_prime = all(num % prime != 0 for prime in list)
+
+        if is_prime:
+            list.append(num)
+
+        num += 1
+
     return list
